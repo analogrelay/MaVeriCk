5e92eaafb060d58a127fbb55170658e0889b93e0
Andrew Nurse <andrew@andrewnurse.net>
2009-04-28 09:14:51 -0700

    Added tests for PrincipalExtensions

---
+++ Tests/Maverick.Web.Tests/Models/PrincipalExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------- 
+// <copyright file="PrincipalExtensionsTests" company="Andrew Nurse">
+//   Copyright (c) 2009 Andrew Nurse.  Licensed under the Ms-PL license: http://opensource.org/licenses/ms-pl.html
+// </copyright>
+// <summary>
+//   Defines the PrincipalExtensionsTests type.
+// </summary>
+// ---------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Principal;
+using Maverick.Web.Models;
+using Microsoft.IdentityModel.Claims;
+
+namespace Maverick.Web.Tests.Models {
+    [TestClass]
+    public class PrincipalExtensionsTests {
+        [TestMethod]
+        public void AsUserIdentity_Returns_UnauthenticatedIdentity_If_Principal_Not_IClaimsPrincipal() {
+            // Arrange
+            IPrincipal principal = new GenericPrincipal(new GenericIdentity("Foo"), new[] {"Bar"});
+            
+            // Act
+            UserIdentity identity = principal.AsUserIdentity();
+
+            // Assert
+            Assert.IsFalse(identity.Authenticated);
+            Assert.IsNull(identity.Principal);
+        }
+
+        [TestMethod]
+        public void AsUserIdentity_Returns_AuthenticatedIdentity_If_Principal_Is_IClaimsPrincipal() {
+            // Arrange
+            IClaimsPrincipal principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim(ClaimTypes.Role, "Foo")));
+
+            // Act
+            UserIdentity identity = principal.AsUserIdentity();
+
+            // Assert
+            Assert.IsTrue(identity.Authenticated);
+            Assert.AreSame(principal, identity.Principal);
+        }
+    }
+}
